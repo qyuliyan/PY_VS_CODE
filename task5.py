91600a3f5f2e4aebe858d9532bcabd0b39575f60
qyuliyan <47634778+qyuliyan@users.noreply.github.com>
2021-10-21 15:50:44 +0300

    Homework update

--- task5.py
+++ task5.py
@@ -1,5 +1,7 @@
 '''
-Doc here
+Задача 5:
+Потребител въвежда израз от типа ((()())()).Напишете скрипт, който проверете
+дали скобите са правилно отворени и затворени.
 '''
 
 def check_bracket_string(brackets_string):
@@ -12,7 +14,7 @@ def check_bracket_string(brackets_string):
         elif c== '(':
             chech_if_brackets_state_is_correct += 1
         else:
-            return (f'Incorrect sybol on position: {position}')
+            return (f'Incorrect sybol "{c}" on position: {position}')
         # print(f'position: {position} - char: {c} ; chech_if_brackets_state_is_correct: {chech_if_brackets_state_is_correct}')
         if  chech_if_brackets_state_is_correct < 0:
             return (f'You have incorrect closing bracket on position {position} in {brackets_string} string.')
@@ -23,8 +25,10 @@ def check_bracket_string(brackets_string):
         return (f'You have {chech_if_brackets_state_is_correct} more openning "(" brackets in {brackets_string}')
 
 if __name__ =="__main__":
-    # input_str = str(')(')
+    # input_str = str('((()())())')
     input_str = input('Enter string with open and close brackets only - "(" ")": ')
     print(check_bracket_string(input_str))
+
+
 
 
